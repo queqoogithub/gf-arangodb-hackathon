9f26af449daadabf1642bc2b9e6c063bcb12eeec
jiissee <jiissee@gmail.com>
2025-03-07 19:41:00 +0000

    try to deploy version 3/8/25

--- api/main.py
+++ api/main.py
@@ -142,4 +142,5 @@ def validate_graph_nodes(nodes: List[GenGraphNode]):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
